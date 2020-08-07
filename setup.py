412d53d6576152dcace2942a29239918258c2a17
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2020-08-07 19:00:32 +0900

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='vim-startup',
+      version='1.0',
+      license="MIT",
+      description='Python Distribution Utilities',
+      author='IK',
+      author_email='ypa.ypa.ypa.t@gmail.com',
+      url='https://github.com/kazukazuinaina/vim-startup',
+      )
