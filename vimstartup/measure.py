03804aeb771f46cac1a0de48637498e46d71909d
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2020-08-08 21:06:00 +0900

    add path checker

--- vimstartup/measure.py
+++ vimstartup/measure.py
@@ -2,11 +2,12 @@ import tempfile
 import os
 from vimstartup.parse import vim_time_parse
 from vimstartup.run_vim import vim_running
+from vimstartup.path_check import checkpath
 
 
 def start_measure():
     with tempfile.TemporaryDirectory() as dname:
-        vim_running('/usr/local/bin/vim', dname + 'vim_start.log')
+        vim_running(checkpath("vim"), dname + 'vim_start.log')
         if os.path.exists(dname + 'vim_start.log'):
             print(vim_time_parse(dname + 'vim_start.log'))
         else:
---
+++ vimstartup/path_check.py
@@ -0,0 +1,5 @@
+import shutil
+
+
+def checkpath(command):
+    return shutil.which(command)
